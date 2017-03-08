12d79d07662ee21d910c1367edafd0458b7e34ea
E.J. Bevenour <ejbevenour@live.com>
2017-03-08 10:37:32 -0500

    updated buildscript

--- utils/buildscript.ps1
+++ utils/buildscript.ps1
@@ -1,23 +1,20 @@
-﻿ param (
+﻿param (
     [Parameter (Mandatory=$true)][string]$address = "127.0.0.1",
     [Parameter(Mandatory=$true)][int]$port = 30120,
-    [string]$password = "lovely",
-    [Parameter (Mandatory=$true)] [string]$projectName = "",
-    [System.IO.DirectoryInfo] $serverRootFolder = [System.IO.DirectoryInfo]("C:\cfx-server"),
-    [Parameter (Mandatory=$true)][string] $sourceDir,
+    [String]$password = "lovely",
+    [Parameter (Mandatory=$true)] [string]$projectName,
+    [Parameter (Mandatory=$true)] [System.IO.DirectoryInfo] $serverRootFolder,
+    [Parameter (Mandatory=$true)] [string] $sourceDir,
     [String] $files
 
- )
- $lfiles = $files.Split(',')
- "Source Dir"
- $y = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList "$sourceDir"
- $h = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList ([System.IO.Path]::Combine($serverRootFolder.FullName,"resources","$projectName\"))
- $h.FullName
+)
+$y = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList "$sourceDir"
+$h = New-Object -TypeName System.IO.DirectoryInfo -ArgumentList ([System.IO.Path]::Combine($serverRootFolder.FullName,"resources","$projectName\"))
+$h.FullName
  
- if($h.Exists.Equals($false)){
-        $h.Create()
- }
- foreach ($file in $lfiles){
-    "[System.IO.Path]::Combine($y.FullName,$file)\"
+if($h.Exists.Equals($false)){
+    $h.Create()
+}
+foreach ($file in $files.Split(',')){
     [System.IO.File]::Copy([System.IO.Path]::Combine($y.FullName,$file),[System.IO.Path]::Combine($h.FullName,$file),$true)
- }
+}
