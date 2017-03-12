e8568d651f061d365ac6579ef6a57673faf7b167
E.J. Bevenour <ejbevenour@live.com>
2017-03-12 17:46:46 -0400

    Updated metadata

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ELSFiveM")]
+[assembly: AssemblyProduct("ELS-for-FiveM")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
