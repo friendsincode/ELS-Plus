1af28e0af6f020e6435e5f7912667c663bc5dde8
E.J. Bevenour <ejbevenour@live.com>
2017-06-02 09:31:25 -0400

    Don't play the sound if it is already playing

--- src/Siren/Tone.cs
+++ src/Siren/Tone.cs
@@ -37,6 +37,7 @@ namespace ELS.Siren
             _state = state;
             if (_state)
             {
+                if (!Audio.HasSoundFinished(soundId)) return;
                 Function.Call(Hash.PLAY_SOUND_FROM_ENTITY, soundId, (InputArgument)_file, (InputArgument)_entity.Handle, (InputArgument)0, (InputArgument)0, (InputArgument)0);
             }
             else
