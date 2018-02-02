2aba51c5efa0cfa478af7756cefd1b0e9fea19a0
ejb1123 <ejb1123@protonmail.com>
2018-02-01 16:53:41 -0800

    fixed ELS-Server.dll filename case.

--- server/ELS-Server/Properties/AssemblyInfo.cs
+++ server/ELS-Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ELS-Server")]
+[assembly: AssemblyTitle("els-server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ELS-Server")]
+[assembly: AssemblyProduct("els-server")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
