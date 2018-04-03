702c7dba43f4acab25707f01a8a6837d3646674c
E.J. Bevenour <ejbevenour@live.com>
2018-04-03 08:12:59 -0400

    update CI Build
    now notifies that dwhen a build is uploaded

--- utils/publish.ps1
+++ utils/publish.ps1
@@ -4,7 +4,8 @@ aws.exe configure set aws_access_key_id $env:aws_key
 aws.exe configure set aws_secret_access_key $env:aws_secret
 aws.exe --endpoint-url $env:aws_host s3 cp $name s3://els-plus/$($env:CI_COMMIT_REF_NAME)/ 2>&1 | Out-Null 
 if($?){
-    "Upload success."
+    utils/notify.ps1 -g "Upload success.
+    Get it at `"https://cdn01.friendsincode.com/els-plus/$($env:CI_COMMIT_REF_NAME)/$($env:CI_PIPELINE_ID)-elsplus.zip`""
 }else {
     "Upload failure."
 }
