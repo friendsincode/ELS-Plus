e3a327c91cf92bef084df7fad56ddeb3cbaeccdd
E.J. Bevenour <ejb1123@internal.fivem-scripts.com>
2018-04-11 06:13:04 -0600

    Update publish.ps1

--- utils/publish.ps1
+++ utils/publish.ps1
@@ -2,7 +2,7 @@ $name="$($env:CI_PIPELINE_ID)-elsplus.zip"
 Compress-Archive elsplus $name
 aws.exe configure set aws_access_key_id $env:aws_key
 aws.exe configure set aws_secret_access_key $env:aws_secret
-aws.exe --endpoint-url $env:aws_host s3 cp $name s3://els-plus/$($env:CI_COMMIT_REF_NAME)/ 2>&1 | Out-Null 
+aws.exe --endpoint-url $env:aws_host s3 cp $name s3://els-plus/$($env:CI_COMMIT_REF_NAME)/
 if($?){
     utils/notify.ps1 -g "Upload success.
     Get it at `"https://cdn01.friendsincode.com/els-plus/$($env:CI_COMMIT_REF_NAME)/$($env:CI_PIPELINE_ID)-elsplus.zip`""
