5c549f1ea46937e052f1bd948de5eef4f700729c
ejb1123 <ejb1123@friendsincode.com>
2019-03-31 13:21:42 -0400

    test dubug build

--- utils/build.ps1
+++ utils/build.ps1
@@ -3,22 +3,33 @@ param(
     [bool]
     $BuildDebug
 )
-pushd src
-$vswherepath=$($PSScriptRoot + "\vswhere.exe")
+Push-Location src
+$vswherepath = $($PSScriptRoot + "\vswhere.exe")
 $installpath = $(&$vswherepath -property installationPath)
-&"$installpath/MSBuild/15.0/Bin/msbuild.exe" /p:outDir=`"$pwd\\tmp\\`" /m /p:Configuration=Release /p:PostBuildEvent= /t:Build ELSPlus.sln
-popd
-
-if(Test-Path -ErrorAction SilentlyContinue -Path "server/ELS-Server/tmp"){
-    cp server/ELS-Server/tmp/* src/tmp
+if ($BuildDebug) {
+    $buildpath = "$pwd\\tmp-debug\\"
 }
-pushd src/tmp
-$pdb2mdb="$PSScriptRoot\..\utils\pdb2mdb\pdb2mdb.exe"
+else {
+    $buildpath = "$pwd\\tmp-release\\"
+}
+if ($BuildDebug) {
+    &"$installpath/MSBuild/15.0/Bin/msbuild.exe" /p:outDir=`"$buildpath`" /m /p:Configuration=Debug /p:PostBuildEvent= /t:Build ELSPlus.sln
+}
+else {
+    &"$installpath/MSBuild/15.0/Bin/msbuild.exe" /p:outDir=`"$buildpath`" /m /p:Configuration=Release /p:PostBuildEvent= /t:Build ELSPlus.sln
+}
+Pop-Location
+
+if (Test-Path -ErrorAction SilentlyContinue -Path "server/ELS-Server/tmp") {
+    Copy-Item server/ELS-Server/tmp/* $buildpath
+}
+Push-Location src/tmp
+$pdb2mdb = "$PSScriptRoot\..\utils\pdb2mdb\pdb2mdb.exe"
 
 "Converting pdbs to mdbs"
 $pdbs = Get-ChildItem "$PWD" -Recurse -Include @('*.dll')
 foreach ($item in $pdbs) {
     $(Invoke-Expression -Command "$pdb2mdb $item" ) 2>&1 | Out-Null 
 }
-rm -Recurse -Force *.pdb
-popd
+Remove-Item -Recurse -Force *.pdb
+Pop-Location
--- utils/publish.ps1
+++ utils/publish.ps1
@@ -1,12 +1,23 @@
-$name="$($env:CI_PIPELINE_ID)-elsplus.zip"
+param(
+    # Build Debug
+    [bool]
+    $BuildDebug
+)
+if ($BuildDebug) {
+    $name = "$($env:CI_PIPELINE_ID)-elsplus.zip"
+}
+else {
+    $name = "$($env:CI_PIPELINE_ID)-elsplus_Debug.zip"
+}
 Compress-Archive elsplus $name
 aws.exe configure set aws_access_key_id $env:aws_key
 aws.exe configure set aws_secret_access_key $env:aws_secret
 aws.exe --endpoint-url $env:aws_host s3 cp $name s3://els-plus/$($env:CI_COMMIT_REF_NAME)/
-if($?){
-    utils/notify.ps1 -g "Upload success.
+if ($?) {
+    notify.ps1 -g "Upload success.
     Get it at `"https://cdn01.friendsincode.com/els-plus/$($env:CI_COMMIT_REF_NAME)/$($env:CI_PIPELINE_ID)-elsplus.zip`""
-}else {
+}
+else {
     "Upload failure."
 }
 
